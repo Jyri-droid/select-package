7d3946ac52016e854348cfe4ad726ed9ff77d7e8
Jyri Konttinen <jyri.konttinen@gmail.com>
2022-11-29 13:07:46 +0200

    add reset button

--- src/Success.js
+++ src/Success.js
@@ -1,6 +1,16 @@
 import Notification from "./Notification";
+import ButtonRow from "./ButtonRow";
+import Submit from "./Submit";
+import { useNavigate } from "react-router-dom";
 
 const Success = (props) => {
+  const navigate = useNavigate();
+  const handleSubmit = (event) => {
+    // Prevent form from clearing
+    event.preventDefault();
+    // Go back to start
+    navigate("/");
+  };
   return (
     <>
       <h1>Select product package</h1>
@@ -9,6 +19,11 @@ const Success = (props) => {
         title="Your selection has been saved"
         list={props.form}
       />
+      <form onSubmit={handleSubmit}>
+        <ButtonRow>
+          <Submit text="Reset flow" />
+        </ButtonRow>
+      </form>
     </>
   );
 };
